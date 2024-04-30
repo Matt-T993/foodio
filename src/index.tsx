f0a399d0f4130266000b2373d2174fafe776b9c5
MattChu993 <53846145+MattChu993@users.noreply.github.com>
2024-04-30 21:28:36 +1000

    fix issue

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles/tailwind.css";
 import "./styles/index.css";
 import "./styles/font.css";
-import { Provider } from "@tyreact-redux";
+import { Provider } from "react-redux";
 import { store } from "./store";
 
 const container = document.getElementById("root");
