242e3ff6268280cc2188f83917357bf58488af25
matthew theseira <matt_.t993@hotmail.com>
2024-05-18 21:52:14 +1000

    ordermenu service is wokring

--- src/pages/Orderonline/index.tsx
+++ src/pages/Orderonline/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { Img, Button, Text, Heading, RatingBar } from "../../components";
 import Footer from "../../components/Footer";
@@ -7,41 +7,76 @@ import OnlineMenu from "components/OnlineMenu";
 import OrderList from "components/OrderList";
 import FoodCategories from "components/FoodCategories";
 import Login from "modals/Login";
+import Service from "service/service";
+
+interface Food {
+  id: string;
+  foodName: string;
+  foodType: string;
+  foodImg: string;
+  description: string;
+  originalPrice: number;
+  categories: string[];
+}
 
 export default function OrderonlinePage() {
   // State to manage modal visibility
   const [isLoginModalOpen, setLoginModalOpen] = useState<boolean>(false);
+  const [foods, setFoods] = useState<Food[]>([]);
+  const [displayFoods, setDisplayFoods] = useState<Food[]>([]);
+
+  useEffect(() => {
+    getFoodsList();
+  }, []);
+
+  const getFoodsList = async () => {
+    try {
+      const data = await Service.fetchAllFoods();
+      setFoods(data);
+      setDisplayFoods(data);
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const filterByFoodType = (foodType: string) => {
+    if (foodType === "All") {
+      setDisplayFoods(foods);
+    } else {
+      const filtered = foods.filter((food) => food.foodType === foodType);
+      setDisplayFoods(filtered);
+    }
+  };
+
+
 
   // Handler to open the modal
   const openLoginModal = () => setLoginModalOpen(true);
 
   // Handler to close the modal
   const closeLoginModal = () => setLoginModalOpen(false);
+
   return (
     <>
       <Helmet>
-        <title>foodio - Order Online </title>
-        <meta
-          name="description"
-          content="Web site created using create-react-app"
-        />
+        <title>foodio - Order Online</title>
+        <meta name="description" content="Web site created using create-react-app" />
       </Helmet>
       <div className="flex flex-col items-center justify-start w-full pt-[51px] gap-[139px] md:pt-5 bg-gray-50">
         <div className="flex flex-col items-center justify-start w-full gap-[103px] md:px-5 max-w-[1112px]">
           <MainNavbar openLoginModal={openLoginModal} />
-
           <div className="flex flex-col items-center justify-start w-full">
             <div className="flex flex-col items-center justify-start w-full gap-[69px]">
               <Heading size="4xl" as="h1" className="!font-opensans">
                 Menu
               </Heading>
               <div className="flex flex-col items-start justify-start w-full gap-[59px]">
-                <FoodCategories />
-                <div className="flex flex-row md:flex-col justify-start items-center w-full gap-[46px] md:gap-5">
-                  <OnlineMenu />
+                <FoodCategories foodType={filterByFoodType} />
+                <div className="flex flex-row md:flex-col justify-start  w-full gap-[46px] md:gap-5">
+                  <OnlineMenu displayFoods={displayFoods}/>
                   <OrderList />
                 </div>
-
                 <div className="flex flex-row sm:flex-col justify-start items-center w-[22%] md:w-full ml-[242px] gap-2.5 md:ml-5 sm:gap-5">
                   <Img
                     src="images/img_arrow_left.svg"
